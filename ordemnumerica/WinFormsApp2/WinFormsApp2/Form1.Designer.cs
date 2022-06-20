0e9408398a97ff67dd54c3975332588eae24f6ef
Laila Matsubara <105072993+LailaMatsubara@users.noreply.github.com>
2022-06-20 17:56:02 -0300

    Add files via upload

---
+++ ordemnumerica/WinFormsApp2/WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtn1 = new System.Windows.Forms.TextBox();
+            this.txtn2 = new System.Windows.Forms.TextBox();
+            this.txtn3 = new System.Windows.Forms.TextBox();
+            this.btncomparar = new System.Windows.Forms.Button();
+            this.btnsair = new System.Windows.Forms.Button();
+            this.btnlimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(350, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ordem crescente";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "insira o primeiro numero";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(66, 255);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "insira o segundo numero";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(66, 355);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "insira o terceiro numero";
+            // 
+            // txtn1
+            // 
+            this.txtn1.Location = new System.Drawing.Point(245, 168);
+            this.txtn1.Name = "txtn1";
+            this.txtn1.Size = new System.Drawing.Size(100, 23);
+            this.txtn1.TabIndex = 4;
+            // 
+            // txtn2
+            // 
+            this.txtn2.Location = new System.Drawing.Point(245, 255);
+            this.txtn2.Name = "txtn2";
+            this.txtn2.Size = new System.Drawing.Size(100, 23);
+            this.txtn2.TabIndex = 5;
+            // 
+            // txtn3
+            // 
+            this.txtn3.Location = new System.Drawing.Point(245, 355);
+            this.txtn3.Name = "txtn3";
+            this.txtn3.Size = new System.Drawing.Size(100, 23);
+            this.txtn3.TabIndex = 6;
+            // 
+            // btncomparar
+            // 
+            this.btncomparar.Location = new System.Drawing.Point(486, 237);
+            this.btncomparar.Name = "btncomparar";
+            this.btncomparar.Size = new System.Drawing.Size(75, 23);
+            this.btncomparar.TabIndex = 7;
+            this.btncomparar.Text = "comparar";
+            this.btncomparar.UseVisualStyleBackColor = true;
+            this.btncomparar.Click += new System.EventHandler(this.btncomparar_Click);
+            // 
+            // btnsair
+            // 
+            this.btnsair.Location = new System.Drawing.Point(640, 177);
+            this.btnsair.Name = "btnsair";
+            this.btnsair.Size = new System.Drawing.Size(75, 23);
+            this.btnsair.TabIndex = 8;
+            this.btnsair.Text = "sair";
+            this.btnsair.UseVisualStyleBackColor = true;
+            this.btnsair.Click += new System.EventHandler(this.btnsair_Click);
+            // 
+            // btnlimpar
+            // 
+            this.btnlimpar.Location = new System.Drawing.Point(640, 329);
+            this.btnlimpar.Name = "btnlimpar";
+            this.btnlimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnlimpar.TabIndex = 9;
+            this.btnlimpar.Text = "limpar";
+            this.btnlimpar.UseVisualStyleBackColor = true;
+            this.btnlimpar.Click += new System.EventHandler(this.btnlimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnlimpar);
+            this.Controls.Add(this.btnsair);
+            this.Controls.Add(this.btncomparar);
+            this.Controls.Add(this.txtn3);
+            this.Controls.Add(this.txtn2);
+            this.Controls.Add(this.txtn1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtn1;
+        private System.Windows.Forms.TextBox txtn2;
+        private System.Windows.Forms.TextBox txtn3;
+        private System.Windows.Forms.Button btncomparar;
+        private System.Windows.Forms.Button btnsair;
+        private System.Windows.Forms.Button btnlimpar;
+    }
+}
+
---
+++ ordemnumerica/WinFormsApp2/WinFormsApp2/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        float n1, n2, n3;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncomparar_Click(object sender, EventArgs e)
+        {
+            n1 = float.Parse(txtn1.Text); 
+            n2 = float.Parse(txtn2.Text);
+            n3 = float.Parse(txtn3.Text); 
+            if (n1 > n2) 
+            { 
+                if (n1 > n3) 
+                { 
+                    if (n2 > n3) 
+                    { 
+                        MessageBox.Show($"Os números em ordem crescente são: {n3}, {n2}, {n1}.", "Ordem crescente", MessageBoxButtons.OK); 
+                    } 
+                    else 
+                    { 
+                        MessageBox.Show($"Os números em ordem crescente são: {n2},{n3},{n1}.", "Ordem crescente", MessageBoxButtons.OK); 
+                    } 
+                } 
+                else 
+                { 
+                    MessageBox.Show($"Os números em ordem crescente são: {n2}, {n1}, {n3}.", "Ordem crescente", MessageBoxButtons.OK); 
+                } 
+            } 
+            else 
+            { 
+                if (n2 > n3) 
+                { 
+                    if (n1 > n3) 
+                    { 
+                        MessageBox.Show($"Os números em ordem crescente são: {n3}, {n1}, {n2}.", "Ordem crescente", MessageBoxButtons.OK); 
+                    } 
+                    else 
+                    { 
+                        MessageBox.Show($"Os números em ordem crescente são: {n1}, {n3}, {n2}.", "Ordem crescente", MessageBoxButtons.OK); 
+                    } 
+                } 
+                else 
+                { 
+                    MessageBox.Show($"Os números em ordem crescente são: {n1}, {n2}, {n3}.", "Ordem crescente", MessageBoxButtons.OK); 
+                } 
+            }
+        }
+
+        private void btnsair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnlimpar_Click(object sender, EventArgs e)
+        {
+            txtn1.Clear(); 
+            txtn2.Clear(); 
+            txtn3.Clear();
+        }
+    }
+}
